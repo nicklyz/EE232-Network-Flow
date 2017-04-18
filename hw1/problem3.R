30670135693d13650d904f49d32c4978f2656a0c
Dong Joon Kim <djkim02@ucla.edu>
2017-04-18 15:07:55 -0700

    Updated problem3.R to plot few more stuffs

--- hw1/problem3.R
+++ hw1/problem3.R
@@ -2,13 +2,13 @@ library(igraph)
 
 # Part (a)
 
-# Creating an evolving random graph with PA exponent of 1 and age exponent of 1
-pa_age_graph <- aging.ba.game(1000, 1, 0, directed = FALSE)
+# Creating an evolving random graph with PA exponent of 1 and age exponent of -1
+pa_age_graph <- aging.ba.game(1000, 1, -1, directed = FALSE)
 plot(pa_age_graph)
 
 # Plotting the degree distribution
 deg_dest <- degree_distribution(pa_age_graph)
-plot(deg_dest)
+barplot(deg_dest, main="Degree Distribution for an Evolving Random Graph")
 
 # Part (b)
 
@@ -17,3 +17,6 @@ community_structure <- fastgreedy.community(pa_age_graph)
 
 # Finding the modularity from the community structure computed with fast greedy method
 modularity <- community_structure$modularity
+max(modularity)
+
+plot(community_structure, pa_age_graph, main="Community Structure for an Evolving Random Graph")
