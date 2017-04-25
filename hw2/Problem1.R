67cc297db4fda5f794da2fd49b8c81724d439d2d
Nick Liu <nickliuyanzhe@gmail.com>
2017-04-25 08:30:54 -0700

    problem1 a

--- hw2/Problem1.R
+++ hw2/Problem1.R
@@ -1,20 +1,12 @@
 library(igraph)
 library(netrw)
 
-# part(a)
-# create undirected random networks with 1000 nodes, with p = 0.01
-g <- random.graph.game(1000, 0.01, directed=FALSE)
-plot(g)
-is.connected(g)
-
-vertex_seq <- netrw(g, 1000, damping = 1, T = 50, output.walk.path = TRUE)$walk.path
-vertex_seq[1,50]
-
-
+# reusable function for creating random network with different size
 random_walk_function = function(num_node, prob) {
   g = random.graph.game(num_node, prob, directed=FALSE)
-  cat("Diameter of the random network with ", num_node, "nodes: ", diameter(g))
-  average_step_t = numeric()
+  cat("Diameter of the random network with", num_node, "nodes: ", diameter(g))
+  average_distance_t = numeric()
+  average_standard_deviation_t = numeric()
   for (t in 1:30) {
     distance_vec = numeric()
     vertex_seq_mat = netrw(graph=g, walker.num = num_node, damping=1, T=t, output.walk.path=TRUE)$walk.path
@@ -32,13 +24,15 @@ random_walk_function = function(num_node, prob) {
       # Append the distance to the distance vector
       distance_vec = c(distance_vec, distance)
     }
-    average_step_t = c(average_step_t, mean(distance_vec))
+    average_distance_t = c(average_distance_t, mean(distance_vec))
+    average_standard_deviation_t = c(average_standard_deviation_t, mean((distance_vec - mean(distance_vec))**2))
   }
-  plot(average_step_t, typ='l', main = paste("Average Steps vs. t - ", n, "nodes"), xlab = "t", ylab = "Average Steps")
+  plot(average_distance_t, typ='l', main = paste("Average Distance vs. t -", num_node, "nodes"), xlab = "Steps", ylab = "Average Distance")
+  plot(average_standard_deviation_t, typ='l', main = paste("Average Standard Deviation vs. t - ", num_node, "nodes"), xlab = "Steps", ylab = "Average Standard Deviation")
 }
 
 
-# Problem 1 - Part A & B #
+# part (a) and part (b) #
 cat("Part a, Random Network with 1000 nodes")
 random_walk_function(1000, 0.01)
 
