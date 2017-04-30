384d5600786f5f67d23ee9427e21c772b6f6e118
Yuanzhi Gao <billmaxwell@ucla.edu>
2017-04-29 17:01:20 -0700

    finished problem 3 and 4

--- hw3/script.R
+++ hw3/script.R
@@ -11,12 +11,8 @@ graph = graph.data.frame(df, directed=TRUE)
 connectivity <- is.connected(graph)
 
 # get giant connected component
-if (!connectivity) {
-  cl <- clusters(graph);
-  gccIndex <- which.max(cl$csize)
-  nonGccNodes <- (1:vcount(graph))[cl$membership != gccIndex]
-  gcc <- delete.vertices(graph, nonGccNodes)
-}
+cluster <- clusters(graph);
+gcc <- induced_subgraph(graph, which(cluster$membership == which.max(cluster$csize)))
 # compare graph size with gcc
 vcount(graph)
 vcount(gcc)
@@ -31,3 +27,27 @@ plot(out_dist, main="Out Degree Distribution of GCC", ylab="distribution", xlab=
 
 
 # NOTE: get weights of the Edges E(g)$Weight
+
+# Problem 3
+# Option 1
+ug1 <- as.undirected(gcc, mode = "each")
+community_propagation1 <- label.propagation.community(ug1, weights=E(ug1)$weights)
+
+# Option 2
+comb_weight <- function(weights) {
+  return (sqrt(prod(weights)))
+}
+
+ug2 <- as.undirected(gcc, mode = "collapse", edge.attr.comb = comb_weight)
+#fastgreedy.community
+community_fastgreedy2 <- fastgreedy.community(ug2, weights = E(ug2)$weights)
+#label.propagation.community
+community_propagation2 <- label.propagation.community(ug2, weights=E(ug2)$weights)
+
+# Problem 4
+largest_community <- induced.subgraph(ug2, which(community_fastgreedy2$membership == which.max(sizes(community_fastgreedy2))))
+sub_community <- fastgreedy.community(largest_community, weights=E(largest_community)$weights)
+sizes(sub_community)
+
+
+
