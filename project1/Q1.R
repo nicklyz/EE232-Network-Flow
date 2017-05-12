f382e9bd2085c38ce8af742ede70ce0155ad7163
Nick Liu <nickliuyanzhe@gmail.com>
2017-05-12 14:50:38 -0700

    Change problem1

--- project1/Q1.R
+++ project1/Q1.R
@@ -9,12 +9,14 @@ is.connected(g)
 diameter(g)
 
 g_degree = degree(g)
-h = hist(g_degree, breaks = seq(from = min(network_degree), to = max(network_degree), by=1),  main = "Histogram of Degree of Facebook Graph", xlab = "Degree", ylab = "Frequency", border="blue", col="green")
-df = data.frame(x=h$mids, y=h$density)
+h = hist(g_degree, breaks = seq(from = min(network_degree), to = max(network_degree), by=1),
+         main = "Histogram of Degree of Facebook Graph", xlab = "Degree", ylab = "Frequency")
 x = h$mids
 y = h$density
 smoothSpline = smooth.spline(x, y, spar=0.35)
-plot(x,y, main="Degree Distribution of Facebook Network")
+plot(x,y, main="Degree Distribution of Facebook Network", xlab = "Degree", ylab = "Frequency")
 lines(smoothSpline, col = "blue")
+cat("Mean Square Error of Fitted Curve")
 mse(y, smoothSpline$y)
-
+cat("Average Degree of the Network")
+mean(g_degree)
