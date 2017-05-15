9612fe9ce3ce83e0c8278990beba92763dd2b356
Dong Joon Kim <djkim02@ucla.edu>
2017-05-14 21:06:45 -0700

    R is so slow

--- project1/Q3-5.R
+++ project1/Q3-5.R
@@ -3,6 +3,7 @@ library(igraph)
 # Reading Facebook graph
 setwd("./Documents/EE 232E/EE232-Network-Flow/project1/")
 facebook_graph <- read.graph("facebook_combined.txt", directed=FALSE)
+facebook_graph$label <- V(facebook_graph)
 
 # Question 3
 
@@ -28,7 +29,7 @@ randomly_picked_core_node <- 349
 # Construct a personal network using the picked core node
 neighborhood_of_core_node <- unlist(neighborhood(facebook_graph, order = 1, nodes = randomly_picked_core_node))
 personal_network <- induced.subgraph(facebook_graph, neighborhood_of_core_node)
-personal_network$label = sort(neighborhood_of_core_node)
+personal_network$label <- sort(neighborhood_of_core_node)
 
 # Fast greedy
 fast_greedy_community <- fastgreedy.community(personal_network)
@@ -54,7 +55,7 @@ neighborhood_without_core_node <- neighborhood_of_core_node[neighborhood_of_core
 
 # Construct a personal network using the picked core node
 personal_network_without_core_node <- induced.subgraph(facebook_graph, neighborhood_without_core_node)
-personal_network_without_core_node$label = sort(neighborhood_without_core_node)
+personal_network_without_core_node$label <- sort(neighborhood_without_core_node)
 
 # Fast greedy
 fast_greedy_community_without_core_node <- fastgreedy.community(personal_network_without_core_node)
@@ -74,3 +75,71 @@ modularity(infomap_community_without_core_node)
 sizes(infomap_community_without_core_node)
 plot(infomap_community_without_core_node, personal_network_without_core_node, edge.color = "light grey", vertex.label=NA, vertex.size=2, asp=9/16)
 
+# Question 5
+# three_picked_core_nodes <- sample(core_nodes, 3)
+
+all_embeddednesses <- numeric()
+all_dispersions <- numeric()
+
+# For each core nodes
+for (each_core_node in core_nodes) {
+  # Create a personal network using the core node
+  neighborhood_of_each_core_node <- unlist(neighborhood(facebook_graph, order = 1, nodes = each_core_node))
+  personal_network_with_each_core_node <- induced.subgraph(facebook_graph, neighborhood_of_each_core_node)
+  personal_network_with_each_core_node$label <- sort(neighborhood_of_each_core_node)
+  each_core_node_neighbors <- neighbors(facebook_graph, each_core_node)
+  
+  for (each_node in personal_network_with_each_core_node$label) {
+    if (each_core_node != each_node) {
+      # Calculating embeddedness of each personal network
+      each_node_neighbors <- neighbors(facebook_graph, each_node)
+      intersection_of_neighbors <- intersect(each_core_node_neighbors, each_node_neighbors)
+      all_embeddednesses <- c(all_embeddednesses, length(intersection_of_neighbors))
+    }
+  }
+}
+
+for (each_core_node in core_nodes) {
+  # Create a personal network using the core node
+  Sys.sleep(0.1)
+  print(each_core_node)
+  neighborhood_of_each_core_node <- unlist(neighborhood(facebook_graph, order = 1, nodes = each_core_node))
+  personal_network_with_each_core_node <- induced.subgraph(facebook_graph, neighborhood_of_each_core_node)
+  personal_network_with_each_core_node$label <- sort(neighborhood_of_each_core_node)
+  each_core_node_neighbors <- neighbors(facebook_graph, each_core_node)
+  
+  for (each_node in personal_network_with_each_core_node$label) {
+    if (each_core_node != each_node) {
+      each_node_neighbors <- neighbors(facebook_graph, each_node)
+      intersection_of_neighbors <- intersect(each_core_node_neighbors, each_node_neighbors)
+      
+      # Removing the node and the core node from the network
+      vertices_to_delete <- c(each_node, each_core_node)
+      facebook_graph_without_each_core_node_and_each_node <- delete.vertices(facebook_graph, vertices_to_delete)
+      facebook_graph_without_each_core_node_and_each_node$label <- sort(intersect(which(facebook_graph$label != each_core_node), which(facebook_graph$label != each_node)))
+      
+      disp <- 0
+      # If they have 1 mutual friend, distance is 0
+      if (length(intersection_of_neighbors) >= 2) {
+        combination_of_intersections = combn(intersection_of_neighbors, 2)
+        for (index in 1:length(combination_of_intersections)) {
+          if (index %% 2) {
+            first_node <- combination_of_intersections[index]
+            second_node <- combination_of_intersections[index + 1]
+            length_of_path <- distances(facebook_graph_without_each_core_node_and_each_node, c(first_node), c(second_node))
+            if (length_of_path[1] != Inf) {
+              disp <- length_of_path[1] + disp
+            }
+          }
+        }
+      }
+      
+      all_dispersions <- c(all_dispersions, disp)
+    }
+  }
+  Sys.sleep(0.1)
+  print(disp)
+}
+
+hist(all_embeddednesses, breaks = 50, main = "Distribution of Embeddedness", xlab = "Embeddedness", col="green")
+hist(all_dispersions, breaks = 50, main = "Distribution of Dispersion", xlab = "Dispersion", col="green")
