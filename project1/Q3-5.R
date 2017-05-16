321bf2724958b38d89174b477741dc21b4a6eb83
Dong Joon Kim <djkim02@ucla.edu>
2017-05-15 20:58:53 -0700

    Done with 3-5

--- project1/Q3-5.R
+++ project1/Q3-5.R
@@ -101,8 +101,6 @@ for (each_core_node in core_nodes) {
 
 for (each_core_node in core_nodes) {
   # Create a personal network using the core node
-  Sys.sleep(0.1)
-  print(each_core_node)
   neighborhood_of_each_core_node <- unlist(neighborhood(facebook_graph, order = 1, nodes = each_core_node))
   personal_network_with_each_core_node <- induced.subgraph(facebook_graph, neighborhood_of_each_core_node)
   personal_network_with_each_core_node$label <- sort(neighborhood_of_each_core_node)
@@ -137,9 +135,97 @@ for (each_core_node in core_nodes) {
       all_dispersions <- c(all_dispersions, disp)
     }
   }
-  Sys.sleep(0.1)
-  print(disp)
 }
 
 hist(all_embeddednesses, breaks = 50, main = "Distribution of Embeddedness", xlab = "Embeddedness", col="green")
 hist(all_dispersions, breaks = 50, main = "Distribution of Dispersion", xlab = "Dispersion", col="green")
+
+for (each_core_node in picked_core_nodes) {
+  # Create a personal network using the core node
+  neighborhood_of_each_core_node <- unlist(neighborhood(facebook_graph, order = 1, nodes = each_core_node))
+  personal_network_with_each_core_node <- induced.subgraph(facebook_graph, neighborhood_of_each_core_node)
+  personal_network_with_each_core_node$label <- sort(neighborhood_of_each_core_node)
+  each_core_node_neighbors <- neighbors(facebook_graph, each_core_node)
+  
+  for (each_node in personal_network_with_each_core_node$label) {
+    if (each_core_node != each_node) {
+      each_node_neighbors <- neighbors(facebook_graph, each_node)
+      intersection_of_neighbors <- intersect(each_core_node_neighbors, each_node_neighbors)
+      
+      # Removing the node and the core node from the network
+      vertices_to_delete <- c(each_node, each_core_node)
+      facebook_graph_without_each_core_node_and_each_node <- delete.vertices(facebook_graph, vertices_to_delete)
+      facebook_graph_without_each_core_node_and_each_node$label <- sort(intersect(which(facebook_graph$label != each_core_node), which(facebook_graph$label != each_node)))
+      
+      disp <- 0
+      # If they have 1 mutual friend, distance is 0
+      if (length(intersection_of_neighbors) >= 2) {
+        combination_of_intersections = combn(intersection_of_neighbors, 2)
+        for (index in 1:length(combination_of_intersections)) {
+          if (index %% 2) {
+            first_node <- combination_of_intersections[index]
+            second_node <- combination_of_intersections[index + 1]
+            length_of_path <- distances(facebook_graph_without_each_core_node_and_each_node, c(first_node), c(second_node))
+            if (length_of_path[1] != Inf) {
+              disp <- length_of_path[1] + disp
+            }
+          }
+        }
+      }
+      
+      all_dispersions <- c(all_dispersions, disp)
+    }
+  }
+}
+
+hist(all_embeddednesses, breaks = 50, main = "Distribution of Embeddedness", xlab = "Embeddedness", col="green")
+hist(all_dispersions, breaks = 50, main = "Distribution of Dispersion", xlab = "Dispersion", col="green")
+
+picked_core_nodes <- c(1, 2048, 2234)
+
+for (each_core_node in picked_core_nodes) {
+  # Create a personal network using the core node
+  each_dispersions <- c()
+  each_embeddedness <- c()
+  neighborhood_of_each_core_node <- unlist(neighborhood(facebook_graph, order = 1, nodes = each_core_node))
+  personal_network_with_each_core_node <- induced.subgraph(facebook_graph, neighborhood_of_each_core_node)
+  personal_network_with_each_core_node$label <- sort(neighborhood_of_each_core_node)
+  each_core_node_neighbors <- neighbors(facebook_graph, each_core_node)
+  
+  for (each_node in personal_network_with_each_core_node$label) {
+    if (each_core_node != each_node) {
+      each_node_neighbors <- neighbors(facebook_graph, each_node)
+      intersection_of_neighbors <- intersect(each_core_node_neighbors, each_node_neighbors)
+      each_embeddednesses <- c(each_embeddednesses, length(intersection_of_neighbors))
+      # Removing the node and the core node from the network
+      vertices_to_delete <- c(each_node, each_core_node)
+      facebook_graph_without_each_core_node_and_each_node <- delete.vertices(facebook_graph, vertices_to_delete)
+      facebook_graph_without_each_core_node_and_each_node$label <- sort(intersect(which(facebook_graph$label != each_core_node), which(facebook_graph$label != each_node)))
+      
+      disp <- 0
+      # If they have 1 mutual friend, distance is 0
+      if (length(intersection_of_neighbors) >= 2) {
+        combination_of_intersections = combn(intersection_of_neighbors, 2)
+        for (index in 1:length(combination_of_intersections)) {
+          if (index %% 2) {
+            first_node <- combination_of_intersections[index]
+            second_node <- combination_of_intersections[index + 1]
+            length_of_path <- distances(facebook_graph_without_each_core_node_and_each_node, c(first_node), c(second_node))
+            if (length_of_path[1] != Inf) {
+              disp <- length_of_path[1] + disp
+            }
+          }
+        }
+      }
+      
+      
+      each_dispersions <- c(each_dispersions, disp)
+    }
+  }
+  
+  dispersion_over_embeddedness <- dispersion / embeddedness
+  
+  max_embeddedness <- personal_network$label[which(each_embeddedness == max(each_embeddedness))]
+  max_dispersion <- personal_network$label[which(dispersion == max(dispersion))]
+  max_dispersion_over_embeddedness <- personal_network$label[which(dispersion_over_embeddedness == max(dispersion_over_embeddedness))]
+}
